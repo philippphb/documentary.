569b91579832c1e4a60565f3593d0f599e38c64c
ElWillibald <el.willi.bal@gmail.com>
2018-05-15 23:45:43 +0200

    updated content and error handling, added referencing of documents in request param to enable links to documents

---
+++ src/main/webapp/js/docpublish.js
@@ -0,0 +1,100 @@
+
+
+// For searching for document events
+var docsOfAuthorEvent;
+
+
+// Initalize the page, externally called from docmain.js
+function initPage() {
+    $('#alert_docsent').hide();
+}
+
+// Login, initially or when the account or network was changed. Eexternally called from ethbasics2.js
+function login() {
+
+    stopWatchers();
+
+    // Clear document list
+    clearBroadcasts("#authordoclist");
+
+    if (isConnected() === false) {
+        showNoEthereumProviderError();
+    }
+
+    else if (isNetworkValid() === false) { 
+        showNetworkNotSupportedError();
+    }
+
+    else {
+
+        // Display contract address
+        $("#val_contract").html("<small>to contract " + contractAddress + " @ " + getNetworkName() + "</small>");
+
+        if (userLoggedIn() === true) {
+
+            // Display account address and load documents authored by the logged in user
+            $("#val_account").html(myaddress.toString() + " @ " + getNetworkName());
+
+            docsOfAuthorEvent = documentaryInstance.DocumentEvent({author: myaddress}, {fromBlock: startBlockNo, toBlock: 'latest'});
+            docsOfAuthorEvent.watch(function(error, result) {
+                if (!error) {
+
+                    // Display only original documents, no comments. Display using summaries and without comment links
+                    if (result.args.refid.toNumber() === 0) {
+                        printDocument(result, "#authordoclist", true, false);
+                        $('#alert_docsent').hide();
+                    }
+                }
+                else {
+                    showError(error);
+                }
+            });        
+        }
+
+        // Handle login to page when no user is logged in in injected provider
+        else {
+            $("#val_account").html("You are not logged in");   
+            showNoUserLoggedInError()
+        }
+    }
+}
+
+// Lougout when site is left, or account or network was changed. Eexternally called from ethbasics2.js
+function logout() {
+}
+
+// Setup watchers. Externally called
+function setUpWatchers() {
+
+}
+
+// Setup watchers. Externally called
+function stopWatchers() {
+    if (docsOfAuthorEvent !== undefined) docsOfAuthorEvent.stopWatching();
+}
+
+// Handle clicks to preview button
+function previewButtonPressed() {
+
+    // Extract document parameters
+    var title = document.getElementById('title').value;
+    var tags = document.getElementById('tags').value;
+    var text = document.getElementById('text').value;
+
+    // Display document in preview modal
+    $('#modal_documentpreview_body').html(createDocumentHtml(0, new Date(), title, myaddress, tags, text, false, false));
+    $('#modal_documentpreview').modal('show');   
+}
+
+// Handle clicks to send button
+function sendDocButtonPressed() {
+
+    // Extract document parameters, refid is always 0 (original document)
+    var refid = 0;
+    var title = document.getElementById('title').value;
+    var tags = document.getElementById('tags').value;
+    var text = document.getElementById('text').value;
+
+    // Submit document to the contract    
+    sendDocument(refid, title, tags, text);
+}
